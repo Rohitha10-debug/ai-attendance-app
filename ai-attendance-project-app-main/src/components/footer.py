e5b0ef668865efa445d3086c987bffc80b359c3b
Rohitha Panchamukhi <rohithawork10@gmail.com>
2026-04-26 21:47:07 +0530

    fixed button alignment and updated UI

--- ai-attendance-project-app-main/src/components/footer.py
+++ ai-attendance-project-app-main/src/components/footer.py
@@ -2,41 +2,20 @@ import streamlit as st
 
 
 def footer_home():
-<<<<<<< HEAD
-    
-    
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by ROHITHA PANCHAMUKHI M </p>  
-       
-=======
-   
-    
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by ROHITHA PANCHAMUKHI M</p>  
-        
->>>>>>> b6650dd (final setup)
+    st.markdown("""
+        <div style="margin-top:2rem; display:flex; justify-content:center; align-items:center;">
+            <p style="font-weight:bold; color:white;">
+                Created with ❤️ by ROHITHA PANCHAMUKHI M
+            </p>
         </div>
-                
-                """, unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
 
 
 def footer_dashboard():
-<<<<<<< HEAD
- 
-    
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by ROHITHA PANCHAMUKHI M</p>  
-=======
-    
-    
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by ROHITHA PANCHAMUKHI M </p>  
->>>>>>> b6650dd (final setup)
-        
+    st.markdown("""
+        <div style="margin-top:2rem; display:flex; justify-content:center; align-items:center;">
+            <p style="font-weight:bold; color:black;">
+                Created with ❤️ by ROHITHA PANCHAMUKHI M
+            </p>
         </div>
-                
-                """, unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
