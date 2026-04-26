8141b3ea27dfdf0e7a29fb231263ee794e9f3372
Rohitha Panchamukhi <rohithawork10@gmail.com>
2026-04-26 14:26:40 +0530

    updated landing image

--- ai-attendance-project-app-main/src/components/footer.py
+++ ai-attendance-project-app-main/src/components/footer.py
@@ -2,50 +2,41 @@ import streamlit as st
 
 
 def footer_home():
-    st.markdown("""
-        <div style="
-            margin-top: 40px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-size: 14px;
-            opacity: 0.85;
-        ">
-            <p style="color: white; margin: 0;">
-                Created with ❤️ by 
-                <span style="
-                    color: #FFD700;
-                    font-weight: 600;
-                    letter-spacing: 0.5px;
-                    margin-left: 4px;
-                ">
-                    Rohitha Panchamukhi M
-                </span>
-            </p>
+<<<<<<< HEAD
+    
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by ROHITHA PANCHAMUKHI M </p>  
+       
+=======
+   
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by ROHITHA PANCHAMUKHI M</p>  
+        
+>>>>>>> b6650dd (final setup)
         </div>
-    """, unsafe_allow_html=True)
+                
+                """, unsafe_allow_html=True)
 
 
 def footer_dashboard():
-    st.markdown("""
-        <div style="
-            margin-top: 40px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-size: 14px;
-            opacity: 0.8;
-        ">
-            <p style="color: black; margin: 0;">
-                Created with ❤️ by 
-                <span style="
-                    color: #ff9900;
-                    font-weight: 600;
-                    letter-spacing: 0.5px;
-                    margin-left: 4px;
-                ">
-                    Rohitha Panchamukhi M
-                </span>
-            </p>
+<<<<<<< HEAD
+ 
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by ROHITHA PANCHAMUKHI M</p>  
+=======
+    
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by ROHITHA PANCHAMUKHI M </p>  
+>>>>>>> b6650dd (final setup)
+        
         </div>
-    """, unsafe_allow_html=True)
+                
+                """, unsafe_allow_html=True)
--- ai-attendance-project-app-main/src/screens/home_screen.py
+++ ai-attendance-project-app-main/src/screens/home_screen.py
@@ -2,33 +2,28 @@ import streamlit as st
 from src.components.header import header_home
 from src.components.footer import footer_home
 from src.ui.base_layout import style_base_layout, style_background_home
-
-
 def home_screen():
 
+
+    header_home()
     style_background_home()
     style_base_layout()
 
-    header_home()
-
-    st.markdown("<br>", unsafe_allow_html=True)
 
     col1, col2 = st.columns(2, gap="large")
 
     with col1:
-        st.markdown("<h3 style='color:#1F1F1F; font-weight:700;'>I'm Student</h3>", unsafe_allow_html=True)
+        st.header("I'm Student")
         st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
-
-        if st.button("Student Portal", use_container_width=True):
-            st.session_state['login_type'] = 'student'
+        if st.button('Student Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
+            st.session_state['login_type']='student'
             st.rerun()
 
     with col2:
-        st.markdown("<h3 style='color:#1F1F1F; font-weight:700;'>I'm Teacher</h3>", unsafe_allow_html=True)
-        st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=130)
-
-        if st.button("Teacher Portal", use_container_width=True):
-            st.session_state['login_type'] = 'teacher'
+        st.header("I'm Teacher")
+        st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=145)
+        if st.button('Teacher Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
+            st.session_state['login_type']='teacher'
             st.rerun()
 
     footer_home()
